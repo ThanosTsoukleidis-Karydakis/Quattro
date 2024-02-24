63c55136aeeded6c5fcbee8cbfa437ba08b6f5e2
Θάνος Τσουκλείδης-Καρυδάκης <106911775+ThanosTsoukleidis-Karydakis@users.noreply.github.com>
2024-02-24 14:00:10 +0200

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"quattrocategorie"
+    host:"b8dbzeqgbjka2mzpkrb2-mysql.services.clever-cloud.com",
+    user:"udfounpy7n1s54z9",
+    password:"bSHnajbd9YgIC4qltq4v",
+    database:"b8dbzeqgbjka2mzpkrb2"
 })
 
 //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
