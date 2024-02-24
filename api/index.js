40318944c1fde51ca2cae8699967a88b38b7b317
Θάνος Τσουκλείδης-Καρυδάκης <106911775+ThanosTsoukleidis-Karydakis@users.noreply.github.com>
2024-02-24 14:20:17 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,8 +3,16 @@ import questionRoutes from "./routes/questions.js"
 import authRoutes from "./routes/auth.js";
 import adminRoutes from "./routes/admin.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express()
+
+const corsOptions = {
+  origin: ["https://quattro.onrender.com", "http://localhost:3000"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
